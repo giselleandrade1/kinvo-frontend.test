3ec40c6752bc7ce7699dd7a07f452e9035d836f4
giselleandrade1 <giselleandradelourenco@gmail.com>
2025-11-17 20:38:06 -0300

    test: import vitest lifecycle helpers in tests

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen, waitFor, fireEvent } from '@testing-library/react'
-import { vi } from 'vitest'
+import { vi, beforeEach, afterEach } from 'vitest'
 import App from '../App'
 
 const mockData = [
